9a3c2b390b094e48bb244dc1dd1715c6ddbb8f07
Thragull <146970246+Thragull@users.noreply.github.com>
2024-02-13 12:24:51 +0000

    Proyecto finalizado

--- src/js/component/Traffic.js
+++ src/js/component/Traffic.js
@@ -11,46 +11,50 @@ const Trafic = () => {
     <>
       <div  id="trafficLights" className='bg-black rounded-4 position-absolute start-50 top-50 translate-middle' >
           <div id="red" className={`rounded-circle bg-danger ${active === "red" ? "activeRed" : ""}`} 
-            onClick={() => {setActive("red");
-            index=0;
+            onClick={() => {
+              index=0;
+              setActive(lights[index]);
             }}>
           </div>
           <div id="yellow" className={`rounded-circle bg-warning ${active === "yellow" ? "activeYellow" : ""}`} 
-            onClick={() => {setActive("yellow");
-            index=1;
+            onClick={() => {
+              index=1;
+              setActive(lights[index]);
             }}>
           </div>
           <div id="green" className={`rounded-circle bg-success ${active === "green" ? "activeGreen" : ""}`} 
             onClick={() => {
-              setActive("green");
               index=2;
+              setActive(lights[index]);
             }}>
           </div>
           <div id="purple" className={`rounded-circle ${newLight ? "" : "visually-hidden"} ${active === "purple" ? "activePurple" : ""}`} 
             onClick={() => {
-            setActive("purple");
             index=3;
+            setActive(lights[index]);
           }}>
           </div>
       </div>
-      <button className='btn btn-primary ' onClick={() => {
-        index= (index+1) % lights.length;
-        setActive(lights[index]);
-      }}>
-        Alternate
-      </button>
-      <button className= {`btn ${newLight? "btn-danger":"btn-success"}`} onClick={() =>{
-        if (newLight){
-          setNewLight(false) 
-          lights.pop()
-        }
-        else{
-          setNewLight(true);
-          lights.push("purple");
-        }
-      }}>
-        {newLight ? "Remove Light" : "Add Light"}
-      </button>
+      <div className='row justify-content-center'>
+        <button className='btn btn-primary col-5 col-md-2 col-xl-1 m-2' onClick={() => {
+          index= (index+1) % lights.length;
+          setActive(lights[index]);
+        }}>
+          Alternate
+        </button>
+        <button className= {`btn col-5 col-md-2 col-xl-1 m-2 ${newLight? "btn-danger":"btn-success"}`} onClick={() =>{
+          if (newLight){
+            setNewLight(false) 
+            lights.pop()
+          }
+          else{
+            setNewLight(true);
+            lights.push("purple");
+          }
+        }}>
+          {newLight ? "Remove Light" : "Add Light"}
+        </button>
+      </div>
     </>
   )
 }
